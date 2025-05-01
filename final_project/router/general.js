5abe1e7ac86c9c22db3e5f176738adaf9cf17de5
joule18 <jrjundam18@gmail.com>
2025-05-01 22:16:07 +0800

    task 5 (get book review) completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,8 +57,11 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+  if (!books.hasOwnProperty(isbn)) {
+    return res.status(300).send("Book not found");
+  }
+  return res.status(300).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
